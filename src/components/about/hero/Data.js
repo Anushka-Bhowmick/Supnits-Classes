22d25a59d4ca7c8ec8f0088a04e4c36e97618fcc
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-11-08 22:46:39 +0530

    color change

--- src/components/about/hero/Data.js
+++ src/components/about/hero/Data.js
@@ -1,20 +1,19 @@
-
 let contact = [
-	{
-		info: 'supnitsclasses@support.com',
-		icon: 'fas fa-envelope',
-		cls: 'e-mail'
-	},
-	{
-		info: '+91 **********',
-		icon: 'fas fa-phone-alt',
-		cls: 'phone-number'
-	},
-	{
-		info: 'address',
-		icon: 'fas fa-map-marker-alt',
-		cls: 'location'
-	}
-]
+  {
+    primary: "supnitsclasses@support.com",
+    icon: "fas fa-envelope",
+    cls: "e-mail",
+  },
+  {
+    primary: "+91 **********",
+    icon: "fas fa-phone-alt",
+    cls: "phone-number",
+  },
+  {
+    primary: "address",
+    icon: "fas fa-map-marker-alt",
+    cls: "location",
+  },
+];
 
-export {contact}
+export { contact };
--- src/components/scrollbtn/Sb.js
+++ src/components/scrollbtn/Sb.js
@@ -1,41 +1,42 @@
+import React, { Component } from "react";
+import "./Sb.css";
 
-import React, {Component} from 'react'
-import './Sb.css'
+class Sb extends Component {
+  state = { s: false };
 
-class Sb extends Component
-{
-	state = {s: false}
+  check_scroll = (_) => {
+    window.scrollY >= 200
+      ? this.setState({ s: true })
+      : this.setState({ s: false });
+  };
 
-    check_scroll = _ =>
-    {
-        (window.scrollY >= 200) ? this.setState({s: true}): this.setState({s: false})
-    }
+  handle_click = (_) => {
+    window.scroll(0, 0);
+  };
 
-    handle_click = _ =>
-    {
-        window.scroll(0, 0)
-    }
+  componentDidMount() {
+    window.addEventListener("scroll", (event) => {
+      this.check_scroll();
+    });
 
-    componentDidMount()
-    {
-        window.addEventListener('scroll', event =>
-        {
-            this.check_scroll()
-        })
+    document.addEventListener("DOMContentLoaded", (event) => {
+      this.check_scroll();
+    });
+  }
 
-        document.addEventListener('DOMContentLoaded', event =>
-        {
-            this.check_scroll()
-        })
-    }
-
-	render()
-	{
-		return (
-			<button className = {`btn btn-danger rounded-circle btn-sm position-fixed ${this.state.s ? 'd-block': 'd-none'}`} id = 'btn-up' onClick = {this.handle_click}><i className="fas fa-arrow-up"></i></button>
-
-		)
-	}
+  render() {
+    return (
+      <button
+        className={`btn btn-primary rounded-circle btn-sm position-fixed ${
+          this.state.s ? "d-block" : "d-none"
+        }`}
+        id="btn-up"
+        onClick={this.handle_click}
+      >
+        <i className="fas fa-arrow-up"></i>
+      </button>
+    );
+  }
 }
 
-export default Sb
+export default Sb;
