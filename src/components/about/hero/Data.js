c6667acc5cc26f0b452036f0791f1dd2d9a1e61f
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-11-08 21:19:50 +0530

    first commit

---
+++ src/components/about/hero/Data.js
@@ -0,0 +1,20 @@
+
+let contact = [
+	{
+		info: 'supnitsclasses@support.com',
+		icon: 'fas fa-envelope',
+		cls: 'e-mail'
+	},
+	{
+		info: '+91 **********',
+		icon: 'fas fa-phone-alt',
+		cls: 'phone-number'
+	},
+	{
+		info: 'address',
+		icon: 'fas fa-map-marker-alt',
+		cls: 'location'
+	}
+]
+
+export {contact}
