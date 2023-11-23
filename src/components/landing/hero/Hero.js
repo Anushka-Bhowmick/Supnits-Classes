c238156be218980af8a76dec7795f8ff6de37448
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-11-23 21:10:07 +0530

    changes

--- src/components/landing/hero/Hero.js
+++ src/components/landing/hero/Hero.js
@@ -18,10 +18,9 @@ class Hero extends Component {
                   we always would be nascent
                 </p>
                 <p className="desc pb-3">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat
+                  Discover excellence in education with Supnits Classes, where
+                  we redefine learning as a family. Our unparalleled courses
+                  cater to all ICSE and CBSE students,JEE and NEET aspirants
                 </p>
                 <div className="buttons">
                   <Link
