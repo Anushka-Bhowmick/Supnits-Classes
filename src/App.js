110ab6c1191e2363fc886f9093dcaddde97651fd
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2024-01-03 20:13:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,27 @@ import Sb from './components/scrollbtn/Sb'
 import Courses from './components/courses/Courses'
 import Contact from './components/contact/Contact'
 import Notfound from './components/notfound/Notfound'
-
+import CarouselComponent from './components/carousel/CarouselComponent'; // Import CarouselComponent
 
 class App extends Component
 {
     render()
     {
+    const carouselImages = [
+      {
+        src: 'https://medicaldialogues.in/h-upload/2022/03/12/500x300_172130-170895-126075-aiims-delhi.webp',
+        alt: 'Image 1',
+        caption: 'Welcome to Supnits Classes',
+        description: 'Learn and Excel in Your Studies',
+      },
+      {
+        src: 'https://static.toiimg.com/thumb/msid-96327181,width-1280,height-720,resizemode-4/96327181.jpg',
+        alt: 'Image 2',
+        caption: 'Explore Our Courses',
+        description: 'Dive into a world of knowledge and education.',
+      },
+      // Add more images as needed
+    ];
   		return (
           <BrowserRouter>
             <>
