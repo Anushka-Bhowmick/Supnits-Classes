2ba2a065da0547efa31a513f45b9639a4e6575ba
Soumojitshome2023 <soumojitshome2021@gmail.com>
2024-01-11 11:36:23 +0530

    Improve Visibility of Homepage Slider Text

--- src/components/landing/Carousel/CarouSel.js
+++ src/components/landing/Carousel/CarouSel.js
@@ -1,4 +1,5 @@
 import Carousel from 'react-bootstrap/Carousel';
+import CarouselSt from './Carousel.module.css';
 
 function CarouSel() {
     return (
@@ -10,8 +11,10 @@ function CarouSel() {
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <div className={CarouselSt.Carousel_Caption}>
+                        <h5>First slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </div>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -21,8 +24,10 @@ function CarouSel() {
                     alt="Second slide"
                 />
                 <Carousel.Caption>
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <div className={CarouselSt.Carousel_Caption}>
+                        <h5>Second slide label</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -32,10 +37,12 @@ function CarouSel() {
                     alt="Third slide"
                 />
                 <Carousel.Caption>
-                    <h5>Third slide label</h5>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
+                    <div className={CarouselSt.Carousel_Caption}>
+                        <h5>Third slide label</h5>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </div>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -45,10 +52,12 @@ function CarouSel() {
                     alt="Third slide"
                 />
                 <Carousel.Caption>
-                    <h5>Fourth slide label</h5>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
+                    <div className={CarouselSt.Carousel_Caption}>
+                        <h5>Fourth slide label</h5>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </div>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
