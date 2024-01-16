be46c7813be26b271eda47b2244488d7aae87c4a
ANAMASGARD <chaudharygaurav2004@gmail.com>
2024-01-16 09:42:00 +0530

    main

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
