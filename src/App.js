f4428581d98e3bf8a87b519289cbe4a7bf271f83
Debangshu Nath <thecrusader.25225@gmail.com>
2024-07-16 21:25:56 +0530

    Fixed "Dark Mode" text color
    Fixed issue of "Dark Mode" written in black color when being in dark mode making it invisible to see

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import Landing from './components/landing/Landing';
-import About from './components/about/About';
-import Blogs from './components/blogs/Blogs';
-import Sb from './components/scrollbtn/Sb';
-import Courses from './components/courses/Courses';
-import Contact from './components/contact/Contact';
-import Notfound from './components/notfound/Notfound';
-import Login from './Login';
-import SignUp from './SignUp';
+import { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Landing from "./components/landing/Landing";
+import About from "./components/about/About";
+import Blogs from "./components/blogs/Blogs";
+import Sb from "./components/scrollbtn/Sb";
+import Courses from "./components/courses/Courses";
+import Contact from "./components/contact/Contact";
+import Notfound from "./components/notfound/Notfound";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 class App extends Component {
   render() {
@@ -18,24 +18,24 @@ class App extends Component {
       <BrowserRouter>
         <>
           <Sb />
-          <div className='_navbar'>
+          <div className="_navbar">
             <Navbar />
           </div>
 
-          <div className='_body'>
+          <div className="_body">
             <Routes>
-              <Route path='/' element={<Landing />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/blogs/*' element={<Blogs />} />
-              <Route path='/courses/*' element={<Courses />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/signup' element={<SignUp />} />
-              <Route path='*' element={<Notfound />} />
+              <Route path="/" element={<Landing />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/blogs/*" element={<Blogs />} />
+              <Route path="/courses/*" element={<Courses />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="*" element={<Notfound />} />
             </Routes>
           </div>
 
-          <div className='_footer'>
+          <div className="_footer">
             <Footer />
           </div>
         </>
