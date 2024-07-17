6d5b38a596031765c4d924b5da6c792d5793e609
Debangshu Nath <thecrusader.25225@gmail.com>
2024-07-17 11:21:23 +0530

    Changed Login page top half

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
@@ -13,23 +13,31 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 
 class App extends Component {
+  state = { darkMode: false };
+
+  toggleDarkMode = () => {
+    this.setState((prevState) => ({ darkMode: !prevState.darkMode }));
+  };
+
   render() {
+    const { darkMode } = this.state;
+
     return (
       <BrowserRouter>
         <>
           <Sb />
           <div className="_navbar">
-            <Navbar />
+            <Navbar darkMode={darkMode} toggleDarkMode={this.toggleDarkMode} />
           </div>
 
-          <div className="_body">
+          <div className={`_body ${darkMode ? "dark-mode" : "light-mode"}`}>
             <Routes>
               <Route path="/" element={<Landing />} />
               <Route path="/about" element={<About />} />
               <Route path="/blogs/*" element={<Blogs />} />
               <Route path="/courses/*" element={<Courses />} />
               <Route path="/contact" element={<Contact />} />
-              <Route path="/login" element={<Login />} />
+              <Route path="/login" element={<Login darkMode={darkMode} />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="*" element={<Notfound />} />
             </Routes>
